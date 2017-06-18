de5c06fcc6510c664129e5f34492f8c2041f5a54
Victor Bonilla <vic.bonpar@gmail.com>
2017-06-18 21:16:23 +0200

    Starting from zero once more. Version 0.1 of controllers and templates

--- src/main/java/videostore/controller/VideoStoreController.java
+++ src/main/java/videostore/controller/VideoStoreController.java
@@ -5,8 +5,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
-import videostore.service.MovieService;
-import videostore.service.UserService;
+//import videostore.service.MovieService;
+//import videostore.service.UserService;
 
 /**
  * Created by victor on 17/06/17.
@@ -15,28 +15,40 @@ import videostore.service.UserService;
 @RequestMapping(value = "/")
 public class VideoStoreController {
 
-    @Autowired
-    private UserService userService;
+//    @Autowired
+//    private UserService userService;
+//
+//    @Autowired
+//    private MovieService movieService;
 
-    @Autowired
-    private MovieService movieService;
+    @RequestMapping(value = "/")
+    public ModelAndView home() {
+        return new ModelAndView("home");
+    }
 
     @RequestMapping(value = "/login")
     public ModelAndView login() {
-        String template = userService.login();
-        return new ModelAndView(template);
+//        String template = userService.login();
+        return new ModelAndView("login");
+    }
+
+    //TODO: Meter /movie/{mmovie_id}
+    @RequestMapping(value = "/movie")
+    public ModelAndView movieDetail(/*@RequestParam String movie_title*/){
+//        String template = movieService.search(movie_title);
+        return new ModelAndView("movieDetail");
     }
 
     @RequestMapping(value = "/movie/search")
     public ModelAndView searchMovie(@RequestParam String movie_title){
-        String template = movieService.search(movie_title);
-        return new ModelAndView(template);
+//        String template = movieService.search(movie_title);
+        return new ModelAndView("movieDetail");
     }
 
     @RequestMapping(value = "/movie/watch")
-    public ModelAndView watchMovie(@RequestParam int movie_id){
-        String template = movieService.watch(movie_id);
-        return new ModelAndView(template);
+    public ModelAndView watchMovie(/*@RequestParam int movie_id*/){
+//        String template = movieService.watch(movie_id);
+        return new ModelAndView("watchMovie");
     }
 
     @RequestMapping(value = "/management")
@@ -48,8 +60,8 @@ public class VideoStoreController {
     public ModelAndView newUser(@RequestParam String user_name,
                                 @RequestParam String user_password,
                                 @RequestParam String user_email) {
-        String template = userService.newUser(user_name, user_password, user_email);
-        return new ModelAndView(template);
+//        String template = userService.newUser(user_name, user_password, user_email);
+        return new ModelAndView("management");
     }
 
     @RequestMapping(value = "/management/user/modify")
@@ -57,14 +69,14 @@ public class VideoStoreController {
                                    @RequestParam String user_name,
                                    @RequestParam String user_password,
                                    @RequestParam String user_email) {
-        String template = userService.modifyUser(id, user_name, user_password, user_email);
-        return new ModelAndView(template);
+//        String template = userService.modifyUser(id, user_name, user_password, user_email);
+        return new ModelAndView("management");
     }
 
     @RequestMapping(value = "/management/user/delete")
     public ModelAndView deleteUser(@RequestParam int id) {
-        String template = userService.deleteUser(id);
-        return new ModelAndView(template);
+//        String template = userService.deleteUser(id);
+        return new ModelAndView("management");
     }
 
     @RequestMapping(value = "/management/movie/new")
@@ -76,8 +88,8 @@ public class VideoStoreController {
                                  @RequestParam(required = false) String movie_actors,
                                  @RequestParam(required = false) String movie_url_front,
                                  @RequestParam(required = false) double movie_rating) {
-        String template = movieService.newMovie(movie_title, movie_url, movie_desc, movie_year, movie_director, movie_actors, movie_url_front, movie_rating);
-        return new ModelAndView(template);
+//        String template = movieService.newMovie(movie_title, movie_url, movie_desc, movie_year, movie_director, movie_actors, movie_url_front, movie_rating);
+        return new ModelAndView("management");
     }
 
     @RequestMapping(value = "/management/movie/modify")
@@ -90,14 +102,14 @@ public class VideoStoreController {
                                     @RequestParam String movie_actors,
                                     @RequestParam String movie_url_front,
                                     @RequestParam double movie_rating) {
-        String template = movieService.modifyMovie(movie_id, movie_title, movie_url, movie_desc, movie_year, movie_director, movie_actors, movie_url_front, movie_rating);
-        return new ModelAndView(template);
+//        String template = movieService.modifyMovie(movie_id, movie_title, movie_url, movie_desc, movie_year, movie_director, movie_actors, movie_url_front, movie_rating);
+        return new ModelAndView("management");
     }
 
     @RequestMapping(value = "/management/movie/delete")
     public ModelAndView deleteMovie(@RequestParam int id) {
-        String template = movieService.deleteMovie(id);
-        return new ModelAndView(template);
+//        String template = movieService.deleteMovie(id);
+        return new ModelAndView("management");
     }
 
 }
